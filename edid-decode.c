d184dea043f023f24f1c4d276074030d60ec2542
Mike Bourgeous <mike@mikebourgeous.com>
2015-02-22 12:28:26 -0700

    Fixed rate display for audio blocks.
    rpavlik notes: Much of the original patch had already been upstreamed,
    so was redundant when rebasing, but the mistaken units for bitrate
    in the output remained, so that is what remained of this patch.

--- edid-decode.c
+++ edid-decode.c
@@ -1100,7 +1100,7 @@ cta_audio_block(unsigned char *x)
 		  (x[i+2] & 0x02) ? " 20" : "",
 		  (x[i+2] & 0x01) ? " 16" : "");
 	} else if (format <= 8) {
-	    printf("      Maximum bit rate: %d kHz\n", x[i+2] * 8);
+	    printf("      Maximum bit rate: %d kbit/s\n", x[i+2] * 8);
 	} else if (format == 14) {
 	    printf("      Profile: %d\n", x[i+2] & 7);
 	} else if ((ext_format >= 4 && ext_format <= 6) ||
